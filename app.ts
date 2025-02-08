9da0ab4681bb9ba7a0d843c9160b98a46b7b8724
moizawan07 <moizahmedawan321@gmail.com>
2025-02-07 21:10:24 -0800

    Quiz_Application

---
+++ app.ts
@@ -0,0 +1,13 @@
+interface userData{
+    name : string;
+    gender: string;
+    email : string;
+    num : number | string 
+}
+
+let num: userData = {
+    name : 'Moiz Ahmed',
+    gender : 'Male',
+    email : 'moiz@gmail.com',
+    num : 2598
+}
